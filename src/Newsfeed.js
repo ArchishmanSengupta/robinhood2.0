1b98ff4047b3d4784a4abf2d67e6df4cc38f63fa
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-02-21 15:37:07 +0530

    newsfeed & stats made

---
+++ src/Newsfeed.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Newsfeed.css'
+function Newsfeed() {
+    return (
+        <div className="newsfeed">
+            <div className="newsfeed__container">
+                <div className="newsfeed_chartSection">
+                    <div className="newsfeed__portfolio">
+                        <h1>$212,678</h1>
+                        <p>+$65.21 (+0.04%) Today
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Newsfeed
