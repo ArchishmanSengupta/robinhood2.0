4b6e4beefc9de52288e53b54d31c170c5782ccac
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-02-23 21:24:18 +0530

    poplarlist with chip made

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -2,7 +2,21 @@ import React from 'react'
 import './Newsfeed.css'
 import LineGraph from './LineGraph'
 import TimeLine from './TimeLine'
+import Chip from './Chip'
+import { Avatar } from "@material-ui/core";
 
+const popularTopics=[
+    "Technology",
+    "Top Movies",
+    "Upcoming Earnings",
+    "Crypto",
+    "Cannabis",
+    "Healthcare Supplies",
+    "Index ETFs",
+    "Technology",
+    "China",
+    "Pharma",
+]
 function Newsfeed() {
     return (
         <div className="newsfeed">
@@ -27,11 +41,26 @@ function Newsfeed() {
                         <p>Market Closed</p>
                         <h1>Happy Thanksgiving</h1>
                     </div>
-
+                </div>
+                <div className="newsfeed__popularlists__section">
+                <div className="newsfeed__popularlists__intro">
+                    <h1>Popular lists</h1>
+                    <p>Show More</p>
+                </div>
+                <div className="newsfeed_popularlists_badges">
+                    {popularTopics.map((topic) => (
+                        <Chip 
+                            // className="topic__badge"
+                            // variant="outlined"
+                            label={topic}
+                            image={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                            />
+                        
+                    ))}
                 </div>
             </div>
-            
         </div>
+    </div>
     );
 }
 
